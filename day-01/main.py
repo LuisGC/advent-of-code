0cd4297fbf80678e045b96f0ac39a5194911585e
Luis GC <luisgc@gmail.com>
2020-12-01 23:19:52 +0100

    day 1, Part 1

---
+++ day-01/main.py
@@ -0,0 +1,17 @@
+def report_repair(inputs):
+    inputs.sort()
+
+    for item in inputs:
+        difference = 2020 - item
+        if difference in inputs:
+            return item * difference
+
+    return false
+
+with open("day-01/example.txt") as f:
+    inputs = [int(line.strip()) for line in f]
+    assert 514579 == report_repair(inputs)
+
+with open("day-01/input.txt") as f:
+    inputs = [int(line.strip()) for line in f]
+    print(report_repair(inputs))
