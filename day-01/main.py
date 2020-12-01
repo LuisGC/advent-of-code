079d839814b6f458fb34034e888291d6db216246
Luis GC <luisgc@gmail.com>
2020-12-01 23:40:40 +0100

    day 1, part 2

--- day-01/main.py
+++ day-01/main.py
@@ -1,18 +1,40 @@
-def report_repair(inputs):
+def report_repair_2(inputs, total):
     inputs.sort()
 
     for item in inputs:
-        difference = 2020 - item
+        difference = total - item
         if difference in inputs:
             return item * difference
 
-    return false
+    return 0
 
 with open("day-01/example.txt") as f:
     inputs = [int(line.strip()) for line in f]
-    assert 514579 == report_repair(inputs)
+    assert 514579 == report_repair_2(inputs, 2020)
 
 with open("day-01/input.txt") as f:
     inputs = [int(line.strip()) for line in f]
     print ("Part 1:")
-    print(report_repair(inputs))
+    print(report_repair_2(inputs, 2020))
+
+
+def report_repair_3(inputs, total):
+    inputs.sort()
+
+    for item in inputs:
+        difference = total - item
+
+        repair_rest = report_repair_2(inputs, difference)
+        if repair_rest != 0:
+            return item * repair_rest
+
+    return 0
+
+with open("day-01/example.txt") as f:
+    inputs = [int(line.strip()) for line in f]
+    assert 241861950 == report_repair_3(inputs, 2020)
+
+with open("day-01/input.txt") as f:
+    inputs = [int(line.strip()) for line in f]
+    print ("Part 2:")
+    print(report_repair_3(inputs, 2020))
