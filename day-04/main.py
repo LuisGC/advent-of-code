3c04d01c5fde5833c7d39074fe0cac30dd285591
Luis GC <luisgc@gmail.com>
2020-12-05 12:50:30 +0100

    day 4 Part 2 completed!

--- day-04/main.py
+++ day-04/main.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import re
 
 Passport = Dict[str, str]
 
@@ -24,11 +25,51 @@ required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 def is_valid(passport: Passport) -> bool:
     return all(field in passport for field in required_fields);
 
+def is_valid_height(hgt: str) -> bool:
+    if hgt.endswith('cm'):
+        hgt = hgt.replace('cm', '')
+        try:
+            return 150 <= int(hgt) <= 193
+        except:
+            return False
+    elif hgt.endswith("in"):
+        hgt = hgt.replace("in", "")
+        try:
+            return 59 <= int(hgt) <= 76
+        except:
+            return False
+
+    return False
+
+def is_valid_and_correct(passport: Passport) -> bool:
+    quality_checks = [
+        1920 <= int(passport.get('byr', -1)) <= 2002,
+        2010 <= int(passport.get('iyr', -1)) <= 2020,
+        2020 <= int(passport.get('eyr', -1)) <= 2030,
+        is_valid_height(passport.get('hgt', '')),
+        re.match(r"^#[0-9a-f]{6}$", passport.get('hcl', '')),
+        passport.get('ecl') in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'),
+        re.match(r"^[0-9]{9}$", passport.get('pid', ''))
+    ]
+
+    return is_valid(passport) and all(quality_checks);
+
 with open("day-04/example.txt") as f:
     passports = parse_list(f.read())
     assert 2 == sum(is_valid(passport) for passport in passports)
 
+with open("day-04/invalid.txt") as f:
+    passports = parse_list(f.read())
+    assert all(is_valid(passport) for passport in passports)
+    assert not any(is_valid_and_correct(passport) for passport in passports)
+
+with open("day-04/valid.txt") as f:
+    passports = parse_list(f.read())
+    assert all(is_valid(passport) for passport in passports)
+    assert all(is_valid_and_correct(passport) for passport in passports)
+
 with open("day-04/input.txt") as f:
     passports = parse_list(f.read())
-    print("Part 1 \nAmount of Passports", len(passports))
-    print("Valid Passports", sum(is_valid(passport) for passport in passports))
+    print("Amount of Passports", len(passports))
+    print("Part 1 - Valid Passports", sum(is_valid(passport) for passport in passports))
+    print("Part 2 - Valid and correct Passports", sum(is_valid_and_correct(passport) for passport in passports))
