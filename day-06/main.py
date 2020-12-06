93cba2bc09a63c6ee6ea8c0f1d084d9e94b571d2
Luis GC <luisgc@gmail.com>
2020-12-06 21:21:28 +0100

    6.2 added

--- day-06/main.py
+++ day-06/main.py
@@ -1,8 +1,21 @@
+from collections import Counter
+
 def count_positive_answer (group: str) -> int:
     yeses = {c for person in group for c in person}
     yeses.discard("\n")
     return len(yeses)
 
+def count_unanimous_answer (group: str) -> int:
+    people = group.strip().split("\n")
+    yeses = Counter(c for person in people for c in person)
+    return sum(count == len(people) for c, count in yeses.items())
+
 with open("day-06/example.txt") as f:
     input =f.read()
     assert 11 == sum(count_positive_answer(group) for group in input.split("\n\n"))
+    assert 6 == sum(count_unanimous_answer(group) for group in input.split("\n\n"))
+
+with open("day-06/input.txt") as f:
+    input =f.read()
+    print("Part 1:",sum(count_positive_answer(group) for group in input.split("\n\n")))
+    print("Part 2:",sum(count_unanimous_answer(group) for group in input.split("\n\n")))
