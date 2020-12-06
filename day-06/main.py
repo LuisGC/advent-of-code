dfdada804af2a09eb495f1fa5ca39d6fa4923b9c
Luis GC <luisgc@gmail.com>
2020-12-06 20:47:08 +0100

    day 6 part 1 simplified

--- day-06/main.py
+++ day-06/main.py
@@ -1,13 +1,8 @@
-def count_positive_answer (input: str) -> int:
-    positive_answers = 0
-
-    for group in input.split("\n\n"):
-        people = group.split("\n")
-        yeses = {c for person in people for c in person}
-        positive_answers += len(yeses)
-
-    return positive_answers
+def count_positive_answer (group: str) -> int:
+    yeses = {c for person in group for c in person}
+    yeses.discard("\n")
+    return len(yeses)
 
 with open("day-06/example.txt") as f:
     input =f.read()
-    assert 11 == count_positive_answer(input)
+    assert 11 == sum(count_positive_answer(group) for group in input.split("\n\n"))
