b9e5210e814aba37bc793c8a059899f0916c476b
Luis GC <luisgc@gmail.com>
2020-12-07 11:34:29 +0100

    minor fix with trailing line

--- day-06/main.py
+++ day-06/main.py
@@ -6,16 +6,16 @@ def count_positive_answer (group: str) -> int:
     return len(yeses)
 
 def count_unanimous_answer (group: str) -> int:
-    people = group.strip().split("\n")
+    people = group.split("\n")
     yeses = Counter(c for person in people for c in person)
     return sum(count == len(people) for c, count in yeses.items())
 
 with open("day-06/example.txt") as f:
-    input =f.read()
+    input =f.read().strip()
     assert 11 == sum(count_positive_answer(group) for group in input.split("\n\n"))
     assert 6 == sum(count_unanimous_answer(group) for group in input.split("\n\n"))
 
 with open("day-06/input.txt") as f:
-    input =f.read()
+    input =f.read().strip()
     print("Part 1:",sum(count_positive_answer(group) for group in input.split("\n\n")))
     print("Part 2:",sum(count_unanimous_answer(group) for group in input.split("\n\n")))
