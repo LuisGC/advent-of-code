d582400857d1770ff795c9a598effac66af5d036
Luis GC <luisgc@gmail.com>
2020-12-09 09:44:39 +0100

    9.1 refactored

--- day-09/main.py
+++ day-09/main.py
@@ -1,8 +1,10 @@
-from typing import List, Iterator
+from typing import List
 from collections import deque
 
-def find_encoding_errors (numbers: List[int], lookback: int = 25) -> Iterator[int]:
+def find_encoding_errors (numbers: List[int], lookback: int = 25) -> List[int]:
     q = deque()
+    result = []
+
     for n in numbers:
         if len(q) < lookback:
             q.append(n)
@@ -12,16 +14,18 @@ def find_encoding_errors (numbers: List[int], lookback: int = 25) -> Iterator[in
                     for j, b in enumerate(q)
                     if i < j}
             if n not in sums:
-                yield n
+                result.append(n)
             q.append(n)
             q.popleft()
+
+    return result
 
 with open("day-09/example.txt") as f:
     raw = f.read().strip()
     numbers = [int(x) for x in raw.split("\n")]
-    assert 127 == next(find_encoding_errors(numbers,5))
+    assert 127 == find_encoding_errors(numbers,5)[0]
 
 with open("day-09/input.txt") as f:
     raw = f.read().strip()
     numbers = [int(x) for x in raw.split("\n")]
-    print("Part 1: The first encoding error is", next(find_encoding_errors(numbers,25)))
+    print("Part 1: The first encoding error is", find_encoding_errors(numbers,25)[0])
