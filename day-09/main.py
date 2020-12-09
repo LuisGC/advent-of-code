e4258e1efe8bed9f97a06b0dd5b497fff022696b
Luis GC <luisgc@gmail.com>
2020-12-09 09:58:15 +0100

    9.2 completed

--- day-09/main.py
+++ day-09/main.py
@@ -1,7 +1,7 @@
 from typing import List
 from collections import deque
 
-def first_encoding_error (numbers: List[int], lookback: int = 25) -> int:
+def first_encoding_error (numbers: List[int], lookback) -> int:
     q = deque()
     result = 0
 
@@ -21,12 +21,34 @@ def first_encoding_error (numbers: List[int], lookback: int = 25) -> int:
 
     return result
 
+def find_encryption_weakness(numbers: List[int], encoding_error: int) -> int:
+
+    range = []
+
+    for i, n in enumerate(numbers):
+        j = i
+        total = n
+        while total < encoding_error and j < len(numbers):
+            j += 1
+            total += numbers[j]
+        if total == encoding_error and i < j:
+            range = numbers[i:j+1]
+
+    if len(range) == 0:
+        raise RuntimeError()
+
+    return min(range) + max(range)
+
 with open("day-09/example.txt") as f:
     raw = f.read().strip()
     numbers = [int(x) for x in raw.split("\n")]
-    assert 127 == first_encoding_error(numbers,5)
+    encoding_error = first_encoding_error(numbers,5)
+    assert 127 == encoding_error
+    assert 62 == find_encryption_weakness(numbers, encoding_error)
 
 with open("day-09/input.txt") as f:
     raw = f.read().strip()
     numbers = [int(x) for x in raw.split("\n")]
-    print("Part 1: The first encoding error is", first_encoding_error(numbers,25))
+    encoding_error = first_encoding_error(numbers,25)
+    print("Part 1: The first encoding error is", encoding_error)
+    print("Part 2: The encryption weakness is", find_encryption_weakness(numbers,encoding_error))
