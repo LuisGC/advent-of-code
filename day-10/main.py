8c774c8c963790e271a4f866fbb6d77a7cf0740e
Luis GC <luisgc@gmail.com>
2020-12-10 09:49:13 +0100

    10.2 completed

--- day-10/main.py
+++ day-10/main.py
@@ -27,19 +27,42 @@ def find_yolt_gaps (adapters: List[int]) -> [int, int]:
     return gaps_1, gaps_3
 
 def count_arrangements(adapters: List[int]) -> int:
-    return 8
+
+    output = adapters[-1]
+
+    # init values
+    arrangements = [0] * (output+1)
+
+    arrangements[0] = 1
+
+    if 1 in adapters:
+        arrangements[1] = 1
+
+    if 2 in adapters:
+        arrangements[2] = 1 + arrangements[1]
+
+    for i in range(3, output + 1):
+        if i not in adapters:
+            continue
+
+        arrangements[i] = arrangements[i-3] + arrangements[i-2] + arrangements[i-1]
+
+    return arrangements[output]
 
 with open("day-10/example.txt") as f:
     adapters = parse_adapters(f.readlines())
     gaps_1, gaps_3 = find_yolt_gaps(adapters)
     assert 35 == gaps_1 * gaps_3
+    assert 8 == count_arrangements(adapters)
 
 with open("day-10/large-example.txt") as f:
     adapters = parse_adapters(f.readlines())
     gaps_1, gaps_3 = find_yolt_gaps(adapters)
     assert 220 == gaps_1 * gaps_3
+    assert 19208 == count_arrangements(adapters)
 
 with open("day-10/input.txt") as f:
     adapters = parse_adapters(f.readlines())
     gaps_1, gaps_3 = find_yolt_gaps(adapters)
     print("Part 1: The 1&3 yolt gaps multiplied is", gaps_1 * gaps_3)
+    print("Part 2: The number of distinct arrangements is", count_arrangements(adapters))
