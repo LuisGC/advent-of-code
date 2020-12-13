58517b001ad519582123842c49cc1e9f66b08693
Luis GC <luisgc@gmail.com>
2020-12-13 10:57:45 +0100

    13.2 code cleaned

--- day-13/main.py
+++ day-13/main.py
@@ -18,30 +18,27 @@ def find_earliest_bus(departure: int, buses: List[int]):
 
 
 def earliest_match(rawbuses: str):
-    print(rawbuses)
 
-    bussIds = [(offset, int(ids))
-              for offset, ids in enumerate(rawbuses.split(","))
-              if ids != "x"]
-    print(bussIds)
+    bussIds = [(offset, int(id))
+               for offset, id in enumerate(rawbuses.split(","))
+               if id != "x"]
 
     time = 0
     step = 1
-    usedSteps = set()
+    matched_buses = set()
     found = False
 
     while not found:
         time += step
-        found = True
-        for offset, bussId in bussIds:
-            if (time+offset) % bussId != 0:
+        for offset, bus_id in bussIds:
+            if (time+offset) % bus_id != 0:
                 found = False
                 break
             else:
-                if bussId not in usedSteps:
-                    print(time, usedSteps)
-                    usedSteps.add(bussId)
-                    step = bussId * step // gcd(bussId, step)
+                found = True
+                if bus_id not in matched_buses:
+                    matched_buses.add(bus_id)
+                    step = bus_id * step // gcd(bus_id, step)
 
     return time
 
@@ -62,7 +59,8 @@ with open("day-13/input.txt") as f:
     departure = int(line1)
     buses = [int(x) for x in line2.split(",") if x != "x"]
     earliest_bus, wait_time = find_earliest_bus(departure, buses)
-    print("Part 1: The earliest wait * bus is", earliest_bus * wait_time)
+    print("Part 1: The earliest wait multiplied by bus ID is",
+          earliest_bus * wait_time)
     earliest_match = earliest_match(line2)
     assert 100000000000000 < earliest_match
     print("Part 2: The earliest match is", earliest_match)
