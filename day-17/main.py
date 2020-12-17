2a91e622513eaa0fca8f494fdfd02ee05d2e6b05
Luis GC <luisgc@gmail.com>
2020-12-17 08:50:54 +0100

    day 17 refactored

--- day-17/main.py
+++ day-17/main.py
@@ -9,19 +9,29 @@ def parse_input(notes: List[str]):
     return lines
 
 
-def apply_cicles(initial_state: List[str], repeats: int) -> int:
-    active = set()
+def read_initial_set(initial_state: List[str], dimensions: int) -> set:
+
+    active_set = set()
 
     for y in range(len(initial_state)):
         for x in range(len(initial_state[0])):
             if initial_state[y][x] == '#':
-                active.add((0, y, x))
+                if dimensions == 3:
+                    active_set.add((0, y, x))
+                else:
+                    active_set.add((0, 0, y, x))
+
+    return active_set
+
+
+def apply_cicles(initial_state: List[str], repeats: int) -> int:
+    active = read_initial_set(initial_state, 3)
 
     for _ in range(repeats):
         minval = min(min(val for val in act) for act in active)
         maxval = max(max(val for val in act) for act in active)
 
-        newact = set()
+        new_actives = set()
 
         for z in range(minval-1, maxval+2):
             for y in range(minval-1, maxval+2):
@@ -37,27 +47,23 @@ def apply_cicles(initial_state: List[str], repeats: int) -> int:
                                 if (z+zdiff, y+ydiff, x+xdiff) in active:
                                     actcount += 1
 
-                    if actcount == 3 or (actcount == 2 and (z, y, x) in active):
-                        newact.add((z, y, x))
+                    if actcount == 3 or (actcount == 2 and (z, y, x)
+                                         in active):
+                        new_actives.add((z, y, x))
 
-        active = newact
+        active = new_actives
 
     return len(active)
 
 
 def apply_cicles_4D(initial_state: List[str], repeats: int) -> int:
-    active = set()
-
-    for y in range(len(initial_state)):
-        for x in range(len(initial_state[0])):
-            if initial_state[y][x] == '#':
-                active.add((0, 0, y, x))
+    active = read_initial_set(initial_state, 4)
 
     for _ in range(repeats):
         minval = min(min(val for val in act) for act in active)
         maxval = max(max(val for val in act) for act in active)
 
-        newact = set()
+        new_actives = set()
 
         for w in range(minval-1, maxval+2):
             for z in range(minval-1, maxval+2):
@@ -76,9 +82,9 @@ def apply_cicles_4D(initial_state: List[str], repeats: int) -> int:
                                             actcount += 1
 
                         if actcount == 3 or (actcount == 2 and (w, z, y, x) in active):
-                            newact.add((w, z, y, x))
+                            new_actives.add((w, z, y, x))
 
-        active = newact
+        active = new_actives
 
     return len(active)
 
@@ -91,5 +97,7 @@ with open("day-17/example.txt") as f:
 
 with open("day-17/input.txt") as f:
     initial_state = parse_input(f.readlines())
-    print("Part 1: The active cubes after 6 repeats are:", apply_cicles(initial_state, 6))
-    print("Part 2: The active cubes after 6 repeats in 4D are:", apply_cicles_4D(initial_state, 6))
+    print("Part 1: The active cubes after 6 repeats are:",
+          apply_cicles(initial_state, 6))
+    print("Part 2: The active cubes after 6 repeats in 4D are:",
+          apply_cicles_4D(initial_state, 6))
