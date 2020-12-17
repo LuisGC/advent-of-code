91f9736ab988b4c12d9503453ce07eb544d728a1
Luis GC <luisgc@gmail.com>
2020-12-17 19:02:24 +0100

    16.1 rule object applied

--- day-16/main.py
+++ day-16/main.py
@@ -13,14 +13,12 @@ def create_range(text: str) -> Range:
     return (int(text.split("-")[0]), int(text.split("-")[1]))
 
 
-def parse_rule(raw_rule: str) -> List[int]:
+def parse_rule(raw_rule: str) -> Rule:
     name, ranges = raw_rule.split(": ")
     r1, r2 = ranges.split(" or ")
 
-    return Rule(
-                name=name,
-                ranges=[create_range(r1), create_range(r2)]
-                )
+    return Rule(name=name,
+                ranges=[create_range(r1), create_range(r2)])
 
 
 def parse_ticket(raw_ticket: str) -> List[int]:
@@ -40,8 +38,7 @@ def parse_input(notes: str):
 
 
 def is_valid_value(value: int, rule: Rule) -> bool:
-    return (rule.ranges[0][0] <= value <= abs(rule.ranges[0][1])
-            or rule.ranges[1][0] <= value <= rule.ranges[1][1])
+    return any(lo <= value <= hi for lo, hi in rule.ranges)
 
 
 def error_rate(rules: List[Rule], tickets: List[int]) -> int:
@@ -62,14 +59,14 @@ def error_rate(rules: List[Rule], tickets: List[int]) -> int:
     return errors
 
 
-def is_valid_ticket(rules: List[int], ticket: List[int]) -> bool:
+def is_valid_ticket(rules: List[Rule], ticket: List[int]) -> bool:
     for n in ticket:
         if not any(is_valid_value(n, rule) for rule in rules):
             return True
     return False
 
 
-def get_valid_tickets(rules: List[int], tickets: List[int]) -> List[int]:
+def get_valid_tickets(rules: List[Rule], tickets: List[int]) -> List[int]:
     valid_tickets = [
         t for t in tickets if not is_valid_ticket(rules, t)
     ]
@@ -79,7 +76,8 @@ def get_valid_tickets(rules: List[int], tickets: List[int]) -> List[int]:
 
 def get_departure_fields(rules: List[int],
                          my_ticket: List[int],
-                         valid_tickets: List[int]):
+                         nearby_tickets: List[int]):
+    valid_tickets = get_valid_tickets(rules, nearby_tickets)
 
     return 1*1*1*1
 
@@ -87,11 +85,9 @@ def get_departure_fields(rules: List[int],
 with open("day-16/example.txt") as f:
     rules, my_ticket, nearby_tickets = parse_input(f.read())
     assert 71 == error_rate(rules, nearby_tickets)
-    valid_tickets = get_valid_tickets(rules, nearby_tickets)
 
 with open("day-16/input.txt") as f:
     rules, my_ticket, nearby_tickets = parse_input(f.read())
     print("Part 1: Error rate is: ", error_rate(rules, nearby_tickets))
-    valid_tickets = get_valid_tickets(rules, nearby_tickets)
     print("Part 2: The departure values multiplied is: ",
           get_departure_fields(rules, my_ticket, nearby_tickets))
