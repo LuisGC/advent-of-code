5092831a64235c854fd8a495754b5b8691d69cef
Luis GC <luisgc@gmail.com>
2020-12-24 10:24:20 +0100

    24.1 completed

---
+++ day-24/main.py
@@ -0,0 +1,39 @@
+from typing import List, Dict
+from collections import defaultdict
+
+directions = {"e": (1, 0), "w": (-1, 0),
+              "se": (0, 1), "sw": (-1, 1),
+              "ne": (1, -1), "nw": (0, -1)}
+
+
+def flip_tiles(transitions: List[str]) -> Dict:
+
+    layout = defaultdict(int)
+
+    for line in transitions:
+        line = line.strip()
+        i = 0
+        x, y = 0, 0
+        while i < len(line):
+            if line[i] in directions:
+                dx, dy = directions[line[i]]
+                i += 1
+            else:
+                dx, dy = directions[line[i:i+2]]
+                i += 2
+            x, y = x + dx, y + dy
+
+        layout[(x, y)] ^= 1
+
+    return layout
+
+
+with open("day-24/example.txt") as f:
+    tile_layout = flip_tiles(f.readlines())
+    assert 10 == sum(v == 1 for v in tile_layout.values())
+
+
+with open("day-24/input.txt") as f:
+    tile_layout = flip_tiles(f.readlines())
+    print("Part 1: The amount of tiles with the black side up is: ",
+          sum(v == 1 for v in tile_layout.values()))
