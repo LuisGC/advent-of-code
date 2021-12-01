a3eea8ec9d7fc5c98ead792a9301323452655ff0
Luis GC <luisgc@gmail.com>
2021-12-01 07:53:30 +0100

    2020 moved to 2020 folder

--- 2020/day-01/main.py
+++ 2020/day-01/main.py
@@ -9,12 +9,12 @@ def report_repair_2(inputs, total):
     return 0
 
 
-with open("day-01/example.txt") as f:
+with open("2020/day-01/example.txt") as f:
     inputs = [int(line.strip()) for line in f]
     assert 514579 == report_repair_2(inputs, 2020)
 
 
-with open("day-01/input.txt") as f:
+with open("2020/day-01/input.txt") as f:
     inputs = [int(line.strip()) for line in f]
     print("Part 1:", report_repair_2(inputs, 2020))
 
@@ -32,11 +32,11 @@ def report_repair_3(inputs, total):
     return 0
 
 
-with open("day-01/example.txt") as f:
+with open("2020/day-01/example.txt") as f:
     inputs = [int(line.strip()) for line in f]
     assert 241861950 == report_repair_3(inputs, 2020)
 
 
-with open("day-01/input.txt") as f:
+with open("2020/day-01/input.txt") as f:
     inputs = [int(line.strip()) for line in f]
     print("Part 2:", report_repair_3(inputs, 2020))
