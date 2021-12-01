fc3cd2f27b11ae9e1587397e726c75003cdc1040
Luis GC <luisgc@gmail.com>
2021-12-01 08:15:54 +0100

    2021.1.2 added

--- 2021/day-01/main.py
+++ 2021/day-01/main.py
@@ -11,9 +11,28 @@ def count_increase(inputs):
 
 with open("2021/day-01/example.txt") as f:
     inputs = [int(line.strip()) for line in f]
-    print(inputs)
-    assert 7 == 0 + count_increase(inputs)
+    assert 7 == count_increase(inputs)
 
 with open("2021/day-01/input.txt") as f:
     inputs = [int(line.strip()) for line in f]
-    print("Part 1:", 0 + count_increase(inputs))
+    print("Part 1:", count_increase(inputs))
+
+
+def count_increase_window(inputs):
+
+    count = 0
+
+    for x in range(0, len(inputs)-3):
+        if inputs[x] + inputs[x+1] + inputs[x+2] < inputs[x+1] + inputs[x+2] + inputs[x+3]:
+            count = count + 1
+
+    return count
+
+
+with open("2021/day-01/example.txt") as f:
+    inputs = [int(line.strip()) for line in f]
+    assert 5 == count_increase_window(inputs)
+
+with open("2021/day-01/input.txt") as f:
+    inputs = [int(line.strip()) for line in f]
+    print("Part 2:", count_increase_window(inputs))
