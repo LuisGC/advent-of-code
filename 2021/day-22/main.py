be81933d3a22374677657013b209ad1cc74066c3
Luis GC <luisgc@gmail.com>
2021-12-22 21:51:16 +0100

    Ranges added

--- 2021/day-22/main.py
+++ 2021/day-22/main.py
@@ -1,20 +1,25 @@
 from typing import List
 
+class Range():
+    def __init__(self, pair: List[str]):
+        self.min = int(pair[0])
+        self.max = int(pair[1])
+
 def parse_input(input: List[str]) -> List:
     steps = []
     for line in input:
         status, coords = line.split(" ")
         cuboid = [x.split('=')[1].split('..') for x in coords.split(',')]
-        steps += [(status == "on", int(cuboid[0][0]), int(cuboid[0][1]), int(cuboid[1][0]), int(cuboid[1][1]), int(cuboid[2][0]), int(cuboid[2][1]))]
+        steps += [(status == "on", Range(cuboid[0]), Range(cuboid[1]), Range(cuboid[2]))]
     return steps
 
 
 def setGrid(steps: List, region_limit: int) -> set:
     grid = set()
-    for st, xmin, xmax, ymin, ymax, zmin, zmax in steps:
-        for x in range(max(xmin, -region_limit), min(xmax, region_limit)+1):
-            for y in range(max(ymin, -region_limit), min(ymax, region_limit)+1):
-                for z in range(max(zmin, -region_limit), min(zmax, region_limit)+1):
+    for st, rangex, rangey, rangez in steps:
+        for x in range(max(rangex.min, -region_limit), min(rangex.max, region_limit)+1):
+            for y in range(max(rangey.min, -region_limit), min(rangey.max, region_limit)+1):
+                for z in range(max(rangez.min, -region_limit), min(rangez.max, region_limit)+1):
                     if st:
                         grid.add((x, y, z))
                     elif (x, y, z) in grid:
@@ -35,7 +40,7 @@ with open("2021/day-22/largest-example.txt") as f:
     steps = parse_input([str(line.strip()) for line in f])
     grid = setGrid(steps, 50)
     assert 474140 == len(grid)
-    # grid = setGrid(steps, 500000)
+    # grid = setGrid(steps, 150000)
     # assert 2758514936282235 == len(grid)
 
 with open("2021/day-22/input.txt") as f:
