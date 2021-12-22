65f822406f5be1947309dda5325c48bf17b4de55
Luis GC <luisgc@gmail.com>
2021-12-22 20:17:04 +0100

    2021.22 limit as a parameter

--- 2021/day-22/main.py
+++ 2021/day-22/main.py
@@ -9,15 +9,12 @@ def parse_input(input: List[str]) -> List:
     return steps
 
 
-def setGrid(steps: List) -> set:
+def setGrid(steps: List, region_limit: int) -> set:
     grid = set()
     for st, xmin, xmax, ymin, ymax, zmin, zmax in steps:
-        for x in range(max(xmin, -50), min(xmax, 50)+1):
-            for y in range(max(ymin, -50), min(ymax, 50)+1):
-                for z in range(max(zmin, -50), min(zmax, 50)+1):
-                    # if not (x >= -50 and x <= 50 and y >= -50 and y <= 50 and z >= -50 and z <= 50):
-                    #     continue
-                    # print(st, x, y, z)
+        for x in range(max(xmin, -region_limit), min(xmax, region_limit)+1):
+            for y in range(max(ymin, -region_limit), min(ymax, region_limit)+1):
+                for z in range(max(zmin, -region_limit), min(zmax, region_limit)+1):
                     if st:
                         grid.add((x, y, z))
                     elif (x, y, z) in grid:
@@ -26,21 +23,23 @@ def setGrid(steps: List) -> set:
 
 with open("2021/day-22/example.txt") as f:
     steps = parse_input([str(line.strip()) for line in f])
-    grid = setGrid(steps)
+    grid = setGrid(steps, 50)
     assert 39 == len(grid)
 
 with open("2021/day-22/larger-example.txt") as f:
     steps = parse_input([str(line.strip()) for line in f])
-    grid = setGrid(steps)
+    grid = setGrid(steps, 50)
     assert 590784 == len(grid)
 
 with open("2021/day-22/largest-example.txt") as f:
     steps = parse_input([str(line.strip()) for line in f])
-    grid = setGrid(steps)
+    grid = setGrid(steps, 50)
     assert 474140 == len(grid)
+    # grid = setGrid(steps, 500000)
+    # assert 2758514936282235 == len(grid)
 
 with open("2021/day-22/input.txt") as f:
     steps = parse_input([str(line.strip()) for line in f])
-    grid = setGrid(steps)
+    grid = setGrid(steps, 50)
     print("Part 1: The amount of cubes is:", len(grid))
 #     print("Part 2: The wins of the player that wins most is:", max(quantum_game.wins))
