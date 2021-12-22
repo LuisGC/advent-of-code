55cc22c530d5b142645f1a1e7a9544cca646aefa
Luis GC <luisgc@gmail.com>
2021-12-22 22:05:21 +0100

    2021.22.1 Now with cuboids

--- 2021/day-22/main.py
+++ 2021/day-22/main.py
@@ -5,35 +5,55 @@ class Range():
         self.min = int(pair[0])
         self.max = int(pair[1])
 
+    def __str__(self):
+        return "<" + str(self.min) + ".." + str(self.max) + ">"
+
+
+class Cuboid():
+    def __init__(self, status: str, rangex: List, rangey: List, rangez: List):
+        self.status = status == "on"
+        self.rangex = Range(rangex)
+        self.rangey = Range(rangey)
+        self.rangez = Range(rangez)
+
+    def __str__(self):
+        string = "Status: " + str(self.status)
+        string += " X=" + str(self.rangex)
+        string += " Y=" + str(self.rangey)
+        string += " Z=" + str(self.rangez)
+        return(string)
+
+
 def parse_input(input: List[str]) -> List:
-    steps = []
+    cuboids = []
     for line in input:
         status, coords = line.split(" ")
         cuboid = [x.split('=')[1].split('..') for x in coords.split(',')]
-        steps += [(status == "on", Range(cuboid[0]), Range(cuboid[1]), Range(cuboid[2]))]
-    return steps
+        cuboids.append(Cuboid(status, cuboid[0], cuboid[1], cuboid[2]))
+
+    return cuboids
 
 
-def setGrid(steps: List, region_limit: int) -> set:
+def setGrid(cuboids: List[Cuboid], region_limit: int) -> set:
     grid = set()
-    for st, rangex, rangey, rangez in steps:
-        for x in range(max(rangex.min, -region_limit), min(rangex.max, region_limit)+1):
-            for y in range(max(rangey.min, -region_limit), min(rangey.max, region_limit)+1):
-                for z in range(max(rangez.min, -region_limit), min(rangez.max, region_limit)+1):
-                    if st:
+    for cuboid in cuboids:
+        for x in range(max(cuboid.rangex.min, -region_limit), min(cuboid.rangex.max, region_limit)+1):
+            for y in range(max(cuboid.rangey.min, -region_limit), min(cuboid.rangey.max, region_limit)+1):
+                for z in range(max(cuboid.rangez.min, -region_limit), min(cuboid.rangez.max, region_limit)+1):
+                    if cuboid.status:
                         grid.add((x, y, z))
                     elif (x, y, z) in grid:
                         grid.remove((x, y, z))
     return grid
 
 with open("2021/day-22/example.txt") as f:
-    steps = parse_input([str(line.strip()) for line in f])
-    grid = setGrid(steps, 50)
+    cuboids = parse_input([str(line.strip()) for line in f])
+    grid = setGrid(cuboids, 50)
     assert 39 == len(grid)
 
 with open("2021/day-22/larger-example.txt") as f:
-    steps = parse_input([str(line.strip()) for line in f])
-    grid = setGrid(steps, 50)
+    cuboids = parse_input([str(line.strip()) for line in f])
+    grid = setGrid(cuboids, 50)
     assert 590784 == len(grid)
 
 with open("2021/day-22/largest-example.txt") as f:
