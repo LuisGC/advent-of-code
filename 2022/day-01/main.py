0bc1785b00ec4cce9beae9723fae81a316de7a24
Luis GC <luisgc@gmail.com>
2022-12-01 08:38:29 +0100

    minor refinement

--- 2022/day-01/main.py
+++ 2022/day-01/main.py
@@ -4,9 +4,9 @@ def max_calories(input_lines):
     elf_calories = 0
 
     for x in input_lines:
+        # print("X:", x)
 
         if x != '' and int(x) != 0:
-            # print("X:", int(x))
             elf_calories += int(x)
 
         else:
@@ -35,7 +35,7 @@ def max_calories_by_3(input_lines):
         else:
             elf_calories += int(x)
 
-    # This should not be necessary, but I am not processing the last blank line
+    # This should not be necessary, but somehow I am not processing the last blank line
     if elf_calories > max_calories[0]:
         max_calories[0] = elf_calories
         max_calories.sort()
@@ -44,7 +44,7 @@ def max_calories_by_3(input_lines):
 
 
 with open("2022/day-01/example.txt") as f:
-    input_lines = [line.strip() for line in f]
+    input_lines = [line.strip() for line in f.readlines()]
     assert 24000 == max_calories(input_lines)
     top_3 = max_calories_by_3(input_lines)
     assert 24000 == max(top_3)
