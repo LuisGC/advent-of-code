5828b95c1d0494e75b42514cfc4b31ef6c440fc1
Luis GC <luisgc@gmail.com>
2022-12-01 21:16:05 +0100

    2022.23 refactored

--- 2021/day-23/main.py
+++ 2021/day-23/main.py
@@ -4,17 +4,17 @@ from sys import maxsize
 COSTS = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
 
 class Burrow:
-    def __init__(self, room_size:int = 2):
+    def __init__(self, room_size: int = 2):
         self.cavern = ['.'] * 11
         self.room_size = room_size
         self.goal = [
             '.', '.', 'A' * self.room_size, '.', 'B' * self.room_size, '.',
             'C' * self.room_size, '.', 'D' * self.room_size, '.', '.'
         ]
-        self.dest = {'A': 2, 'B': 4, 'C': 6, 'D': 8}
-        self.destination_set = set(self.dest.values())
+        self.destination = {'A': 2, 'B': 4, 'C': 6, 'D': 8}
+        self.destination_set = set(self.destination.values())
 
-    def add_line(self, line:str):
+    def add_line(self, line: str):
         for pos, char in enumerate(line):
             if char in ['A', 'B', 'C', 'D']:
                 if self.cavern[pos - 1] != '.':
@@ -25,18 +25,18 @@ class Burrow:
     def __repr__(self):
         return f'{self.cavern}'
 
-    def get_piece_from_room(self, room):
+    def get_piece_from_room(self, room: str):
         for char in room:
             if char != '.':
                 return char
 
-    def add_to_room(self, char, room):
+    def add_to_room(self, char: str, room: str):
         room = list(room)
         distance = room.count('.')
         room[distance - 1] = char
         return ''.join(room), distance
 
-    def can_reach(self, cavern, position, destination):
+    def can_reach(self, cavern: List, position: int, destination: int):
         for i in range(min(position, destination), max(position, destination) + 1):
             if i == position:
                 continue
@@ -46,29 +46,29 @@ class Burrow:
                 return False
         return True
 
-    def room_ok(self, cavern, piece, destination):
+    def room_ok(self, cavern: List, piece: str, destination: int):
         in_room = cavern[destination]
         return len(in_room) == in_room.count('.') + in_room.count(piece)
 
-    def possible_moves(self, cavern, position):
+    def possible_moves(self, cavern: List, position: int):
         piece = cavern[position]
         if position not in self.destination_set:
-            dest = self.dest[piece]
+            dest = self.destination[piece]
             if self.can_reach(cavern, position, dest) and self.room_ok(cavern, piece, dest):
                 return [dest]
             
             return []
         moving_letter = self.get_piece_from_room(piece)
-        if position == self.dest[moving_letter] and self.room_ok(cavern, moving_letter, position):
+        if position == self.destination[moving_letter] and self.room_ok(cavern, moving_letter, position):
             return []
 
         possible = []
         for dest in range(len(cavern)):
             if dest == position:
                 continue
-            if dest in self.destination_set and self.dest[moving_letter] != dest:
+            if dest in self.destination_set and self.destination[moving_letter] != dest:
                 continue
-            if self.dest[moving_letter] == dest:
+            if self.destination[moving_letter] == dest:
                 if not self.room_ok(cavern, moving_letter, dest):
                     continue
             if self.can_reach(cavern, position, dest):
@@ -77,7 +77,7 @@ class Burrow:
         return possible
 
 
-    def move(self, cavern, position, destination):
+    def move(self, cavern: List, position: int, destination: int):
         new_cavern = cavern[:]
         distance = 0
         moving_char = self.get_piece_from_room(cavern[position])
@@ -128,12 +128,13 @@ class Burrow:
         
         return states[tuple(self.goal)]
 
+
 def process_input(input_lines: List, room_size: int = 2) -> Burrow:
     burrow = Burrow(room_size)
     for line in input_lines:
         burrow.add_line(line)
 
-    print(burrow)
+    # print(burrow)
     return burrow
 
 
@@ -149,6 +150,6 @@ with open("2021/day-23/unfolded_example.txt") as f:
     burrow = process_input([line for line in f], 4)
     assert 44169 == burrow.solve()
 
-with open("2021/day-23/unfolded_input.txt") as f:
-    burrow = process_input([line for line in f], 4)
-    print("Part 2: The minimum energy is:", burrow.solve())
+# with open("2021/day-23/unfolded_input.txt") as f:
+#     burrow = process_input([line for line in f], 4)
+#     print("Part 2: The minimum energy is:", burrow.solve())
