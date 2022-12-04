c430ba1dd94998b9d34839f2a06cd1c58c2bb886
Luis GC <luisgc@gmail.com>
2022-12-04 08:04:11 +0100

    2022.4 solved!

--- 2022/day-04/main.py
+++ 2022/day-04/main.py
@@ -12,12 +12,27 @@ def count_fully_contained(elven_pairs: List) -> int:
 
     return fully_contained
 
+def count_overlapped(elven_pairs: List) -> int:
+
+    overlapped = 0
+    for pair in elven_pairs:
+        first, second = pair.split(",")
+        first_min, first_max = first.split("-")
+        second_min, second_max = second.split("-")
+        first_sections = set(list(range(int(first_min), int(first_max) + 1)))
+        second_sections = set(list(range(int(second_min), int(second_max) + 1)))
+        if len(first_sections & second_sections):
+            overlapped += 1
+
+    return overlapped
+
+
 with open("2022/day-04/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     assert 2 == count_fully_contained(input_lines)
-    # assert 70 == common_badge_priority(input_lines)
+    assert 4 == count_overlapped(input_lines)
 
 with open("2022/day-04/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     print("Part 1: Count of fully overlapped is:", count_fully_contained(input_lines))
-#     print("Part 2: Priority of the common badges is ", common_badge_priority(input_lines))
+    print("Part 2: Count of overlapped is ", count_overlapped(input_lines))
