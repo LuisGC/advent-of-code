c0f2e764588a8670655fe5feebb06685531629ef
Luis GC <luisgc@gmail.com>
2022-12-05 08:19:44 +0100

    2022.05 solved!!

--- 2022/day-05/main.py
+++ 2022/day-05/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def execute_instructions(lines: List) -> str:
+def execute_instructions(lines: List, crate_mover: int=9000) -> str:
     split_pos = next(i for i, line in enumerate(lines) if "1" in line)
 
     stacks= [[], [], [], [], [], [], [], [], []]
@@ -13,16 +13,24 @@ def execute_instructions(lines: List) -> str:
 
     for (amount, source, dest) in (instructions):
         # print(stacks)
-        for i in range(amount):
-            stacks[dest - 1].extend(stacks[source - 1][-1:])
-            stacks[source - 1] = stacks[source - 1][:-1]
+        if crate_mover == 9000:
+            for i in range(amount):
+                stacks[dest - 1].extend(stacks[source - 1][-1:])
+                stacks[source - 1] = stacks[source - 1][:-1]
+        else:
+            stacks[dest - 1].extend(stacks[source - 1][-amount:])
+            stacks[source - 1] = stacks[source - 1][:-amount]
+
 
     # print(stacks)
     return "".join(stack[-1] for stack in stacks if stack)
 
 with open("2022/day-05/example.txt", encoding="utf-8") as f:
-    
-    assert "CMZ" == execute_instructions(f.readlines())
+    lines = f.readlines()
+    assert "CMZ" == execute_instructions(lines)
+    assert "MCD" == execute_instructions(lines, 9001)
 
 with open("2022/day-05/input.txt", encoding="utf-8") as f:
-    print("Part 1: Final crates on top are :", execute_instructions(f.readlines()))
+    lines = f.readlines()
+    print("Part 1: Final crates on top with CrateMover 9000 are :", execute_instructions(lines))
+    print("Part 2: Final crates on top with CrateMover 9001 are :", execute_instructions(lines, 9001))
