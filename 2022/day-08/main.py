a8daa8f89b89fca9b61836eab281c1ab7b6e843b
Luis GC <luisgc@gmail.com>
2022-12-08 08:21:05 +0100

    2022.08 solved!!

--- 2022/day-08/main.py
+++ 2022/day-08/main.py
@@ -14,10 +14,35 @@ def count_visible_trees(trees: np.ndarray) -> int:
 
     return visible
 
+def highest_scenic_score(trees: np.ndarray) -> int:
+    highest = 0
+
+    for row in range(1, trees.shape[0] - 1):
+        for col in range(1, trees.shape[1] - 1):
+            left = row - 1
+            while left > 0 and trees[row,col] > trees[left, col]:
+                left -= 1
+            right = row + 1
+            while right < trees.shape[0] - 1 and trees[row,col] > trees[right, col]:
+                right += 1
+            up = col - 1
+            while up > 0 and trees[row, col] > trees[row, up]:
+                up -= 1
+            down = col + 1
+            while down < trees.shape[1] - 1 and trees[row, col] > trees[row, down]:
+                down += 1
+
+            highest = max(highest, (row - left) * (right - row) * (col - up) * (down - col))
+
+    return highest
+
+
 with open("2022/day-08/example.txt", encoding="utf-8") as f:
     trees = [[int(x) for x in line.strip()] for line in f.readlines()]
     assert 21 == count_visible_trees(np.array(trees))
+    assert 8 == highest_scenic_score(np.array(trees))
 
 with open("2022/day-08/input.txt", encoding="utf-8") as f:
     trees = [[int(x) for x in line.strip()] for line in f.readlines()]
     print("Part 1: Count of visible trees is:", count_visible_trees(np.array(trees)))
+    print("Part 2: Highest scenic score is:", highest_scenic_score(np.array(trees)))
