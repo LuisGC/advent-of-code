251ebfd91ee2eda6559eee29e38e0626a0fb0eb6
Luis GC <luisgc@gmail.com>
2022-12-10 08:30:46 +0100

    parse input execution is not needed

--- 2022/day-10/main.py
+++ 2022/day-10/main.py
@@ -1,16 +1,5 @@
 from typing import List
 
-def parse_input(lines: List) -> List:
-    instructions = []
-    for line in lines:
-        parts = line.split()
-        if parts[0] == "noop":
-            instructions.append((parts[0], 0))
-        else: # addx
-            instructions.append((parts[0], int(parts[1])))
-
-    return instructions
-
 def execute_instructions(instructions: List, show_signal:bool=True) -> int:
     MEASURE_CYCLES = [20, 60, 100, 140, 180, 220]
     signal_strenght = 0
@@ -37,25 +26,23 @@ def execute_instructions(instructions: List, show_signal:bool=True) -> int:
             x_register += queued_instructions
             queued_instructions = None
         else:
-            instruction = next(instructions_iterator)
+            instruction = next(instructions_iterator).split()
 
             if instruction[0] == "noop":
                 continue
             else:
-                queued_instructions = instruction[1]
+                queued_instructions = int(instruction[1])
 
     # print(signal_strenght)
     return signal_strenght
 
 
 with open("2022/day-10/example.txt", encoding="utf-8") as f:
-    input_lines = [line.strip() for line in f.readlines()]
-    instructions = parse_input(input_lines)
+    instructions = [line.strip() for line in f.readlines()]
 
     assert 13140 == execute_instructions(instructions, False)
 
 with open("2022/day-10/input.txt", encoding="utf-8") as f:
-    input_lines = [line.strip() for line in f.readlines()]
-    instructions = parse_input(input_lines)
+    instructions = [line.strip() for line in f.readlines()]
 
     print("Part 1: total signal strenght is:", execute_instructions(instructions))
