630a5083431fb6a21ccc02be69896fc2559eb9a7
Luis GC <luisgc@gmail.com>
2022-12-13 17:25:53 +0100

    2022.13 solved!!

--- 2022/day-13/main.py
+++ 2022/day-13/main.py
@@ -1,4 +1,6 @@
 from typing import List
+from math import prod
+from functools import cmp_to_key
 
 def compare_packets(left, right) -> int:
     if isinstance(left, int) and isinstance(right, int):
@@ -21,12 +23,23 @@ def pairs_in_order(packets: List) -> List:
             right_pairs.append(i + 1)
     return right_pairs
 
+def sort_packets(packets: List) -> List:
+    keys = [[[2]], [[6]]]
+    keys_index = []
+    for i, packet in enumerate(sorted(packets + keys, key=cmp_to_key(compare_packets))):
+        if packet in keys:
+            keys_index.append(i + 1)
+
+    return keys_index
+
 with open("2022/day-13/example.txt", encoding="utf-8") as f:
     packets = [eval(line) for line in f.read().splitlines() if len(line)]
 
     assert 13 == sum(pairs_in_order(packets))
+    assert 140 == prod(sort_packets(packets))
 
 with open("2022/day-13/input.txt", encoding="utf-8") as f:
     packets = [eval(line) for line in f.read().splitlines() if len(line)]
 
     print("Part 1: Sum of indices of pairs in rigth order:", sum(pairs_in_order(packets)))
+    print("Part 2: Prod of indices of key packets:", prod(sort_packets(packets)))
