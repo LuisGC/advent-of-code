135403bc16b2dd2a853588becbcb444220ae85a7
Luis GC <luisgc@gmail.com>
2022-12-21 08:34:19 +0100

    brute force does not work here

--- 2022/day-21/main.py
+++ 2022/day-21/main.py
@@ -10,7 +10,7 @@ def parse_input(lines: List) -> dict:
             monkeys[parts[0]] = (parts[1], parts[2], parts[3])
     return monkeys
 
-def hear_monkeys(monkeys: dict, leader: str) -> int:
+def hear_monkeys(monkeys: dict, leader: str='root') -> int:
     shouts = {}
     leader_found = False
 
@@ -38,14 +38,55 @@ def hear_monkeys(monkeys: dict, leader: str) -> int:
     return shouts[leader]
 
 
+def guess_my_number(monkeys: dict, leader: str='root', my_name: str='humn') -> int:
+    shouts = {}
+    leader_found = False
+    my_number = 0 # >63510129 for test and did not finish
+
+    while not leader_found:
+        for monkey_name, monkey_value in monkeys.items():
+            if monkey_name == leader:
+                if monkey_value[0] in shouts and monkey_value[2] in shouts:
+                    if monkey_value[0] == monkey_value[2]:
+                        leader_found = True
+                        break
+                    else:
+                        shouts = {}
+                        my_number += 1
+            elif monkey_name == my_name:
+                shouts[monkey_name] = my_number
+            elif len(monkey_value) == 1:
+                shouts[monkey_name] = monkey_value[0]
+            else:
+                if monkey_value[0] in shouts and monkey_value[2] in shouts:
+                    if monkey_value[1] == '+':
+                        shouts[monkey_name] = shouts[monkey_value[0]] + shouts[monkey_value[2]]
+                    elif monkey_value[1] == '-':
+                        shouts[monkey_name] = shouts[monkey_value[0]] - shouts[monkey_value[2]]
+                    elif monkey_value[1] == '*':
+                        shouts[monkey_name] = shouts[monkey_value[0]] * shouts[monkey_value[2]]
+                    elif monkey_value[1] == '/':
+                        shouts[monkey_name] = shouts[monkey_value[0]] // shouts[monkey_value[2]]
+                    else:
+                        raise ValueError("Invalid operation")
+
+                    if monkey_name == leader:
+                        leader_found = True
+                        break
+    
+    return my_number
+
+
 with open("2022/day-21/example.txt", encoding="utf-8") as f:
     lines = [line.strip() for line in f.readlines()]
     monkeys = parse_input(lines)
 
-    assert 152 == hear_monkeys(monkeys, 'root')
+    assert 152 == hear_monkeys(monkeys)
+    assert 31 == guess_my_number(monkeys)
 
 with open("2022/day-21/input.txt", encoding="utf-8") as f:
     lines = [line.strip() for line in f.readlines()]
     monkeys = parse_input(lines)
 
-    print("Part 1: The number of root is:", hear_monkeys(monkeys, 'root'))
+    print("Part 1: The number of root is:", hear_monkeys(monkeys))
+    print("Part 2: The number of humn must be:", guess_my_number(monkeys))
