1ab8524c50d29f69cf366a5fc37c42f44b4c66ce
Luis GC <luisgc@gmail.com>
2023-12-03 08:43:04 +0100

    2023-03 solved

--- 2023/day-03/main.py
+++ 2023/day-03/main.py
@@ -1,14 +1,19 @@
 from itertools import product
+from typing import Tuple
+from collections import defaultdict
+from math import prod
 
 def is_part(char: str) -> bool:
     return char not in ".0123456789"
 
-def get_part_numbers(lines: list) -> int:
+def get_part_numbers(lines: list) -> Tuple[int,int]:
     total = 0
+    all_gears = defaultdict(list)
 
     for i in range(len(lines)):
         initial_digit_pos = None
         near_part = False
+        gears = set()
 
         for j in range(len(lines[i]) + 1):
             if j < len(lines[i]) and lines[i][j].isdigit():
@@ -22,22 +27,31 @@ def get_part_numbers(lines: list) -> int:
                     if 0 <= si < len(lines) and 0 <= sj < len(lines[si]):
                         if is_part(lines[si][sj]):
                             near_part = True
+                        if lines[si][sj] == "*":
+                            gears.add((si, sj))
             else:
                 if initial_digit_pos is not None:
+                    number = int(lines[i][initial_digit_pos:j])
                     if near_part:
-                        total += int(lines[i][initial_digit_pos:j])
-                    
+                        total += number
+                    for gear in gears:
+                        all_gears[gear].append(number)
+
                     initial_digit_pos = None
                     near_part = False
+                    gears = set()
 
-    return total
+    return total, sum(prod(elts) for elts in all_gears.values() if len(elts) >= 2)
 
 
 with open("2023/day-03/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-    assert 4361 == get_part_numbers(input_lines)
+    total_part_numbers, sum_all_gear_ratios = get_part_numbers(input_lines)
+    assert 4361 == total_part_numbers
+    assert 467835 == sum_all_gear_ratios
 
 with open("2023/day-03/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-
-    print("Part 1: Sum of all part numbers is ", get_part_numbers(input_lines))
+    total_part_numbers, sum_all_gear_ratios = get_part_numbers(input_lines)
+    print("Part 1: Sum of all part numbers is ", total_part_numbers)
+    print("Part 2: Sum of all gear ratios is ", sum_all_gear_ratios)
