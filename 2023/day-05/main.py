67c782e7f75a8054c74a7122e93dbc492f112e62
Luis GC <luisgc@gmail.com>
2023-12-05 22:34:42 +0100

    2023-05 solved!!

--- 2023/day-05/main.py
+++ 2023/day-05/main.py
@@ -1,7 +1,7 @@
 import sys
 from typing import Tuple, List
 
-def parse_input (lines: list) -> Tuple[list, list]:
+def parse_input (lines: List[str]) -> Tuple[List[int], List[int]]:
 
     seeds = [
         int(number) for number in lines[0].strip().split(": ")[-1].split()
@@ -31,16 +31,56 @@ def find_lowest_location(seeds: List[int], mappings: List[int]) -> int:
 
     return lowest
 
+def find_lowest_location_with_ranges(seeds: List[int], mappings: List[int]) -> int:
+    seed_ranges = [
+        (start, start + length) for start, length in zip(seeds[0::2], seeds[1::2])
+    ]
+    candidates = [[] for _ in range(len(mappings))]
+
+    for range_start, range_end in seed_ranges:
+        ranges = [(range_start, range_end)]
+
+        for i, mapping in enumerate(mappings):
+            while ranges:
+                range_start, range_end = ranges.pop()
+
+                for destination_start, source_start, range_length in mapping:
+                    source_end = source_start + range_length
+                    offset = destination_start - source_start
+
+                    if source_end <= range_start or range_end <= source_start:
+                        continue
+
+                    if range_start < source_start:
+                        ranges.append((range_start, source_start))
+                        range_start = source_start
+
+                    if source_end < range_end:
+                        ranges.append((source_end, range_end))
+                        range_end = source_end
+                    
+                    range_start += offset
+                    range_end += offset
+
+                    break
+                
+                candidates[i].append((range_start, range_end))
+
+            ranges = candidates[i]
+
+    return min(candidates[-1])[0]
+
 
 with open("2023/day-05/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     seeds, mappings = parse_input(input_lines)
     
     assert 35 == find_lowest_location(seeds, mappings)
+    assert 46 == find_lowest_location_with_ranges(seeds, mappings)
 
 with open("2023/day-05/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     seeds, mappings = parse_input(input_lines)
     
     print("Part 1: Lowest location is ", find_lowest_location(seeds, mappings))
-    # print("Part 2: Sum of all gear ratios is ", sum_all_gear_ratios)
+    print("Part 2: Lowest location with seed ranges is ", find_lowest_location_with_ranges(seeds, mappings))
