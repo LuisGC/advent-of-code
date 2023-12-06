3e1d4287ae845e6205aa869e28fc896252535ec0
Luis GC <luisgc@gmail.com>
2023-12-06 07:08:43 +0100

    2023-06 solved!!

--- 2023/day-06/main.py
+++ 2023/day-06/main.py
@@ -9,7 +9,16 @@ def parse_input (lines: List[str]) -> Tuple[List[int], List[int]]:
     for item in lines[1].split()[1:]:
         distances.append(int(item))
 
-    return times,distances
+    return times, distances
+
+def parse_input_kerning(lines: List[str]) -> Tuple[List[int], List[int]]:
+    times = ''
+    distances = ''
+    for item in lines[0].split()[1:]:
+        times += item
+    for item in lines[1].split()[1:]:
+        distances += item
+    return [int(times)], [int(distances)]
 
 def distance_travelled(max_time: int, button_holding_time: int) -> int:
     distance = (max_time - button_holding_time) * button_holding_time
@@ -35,11 +44,17 @@ def record_options(times: List[int], distances: List[int]) -> List[int]:
 with open("2023/day-06/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     times, distances = parse_input(input_lines)
-    
     assert 288 == prod(record_options(times, distances))
+    
+    times, distances = parse_input_kerning(input_lines)
+    assert 71503 == record_options(times, distances)[0]
+
 
 with open("2023/day-06/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-    times, distances = parse_input(input_lines)
     
+    times, distances = parse_input(input_lines)
     print("Part 1: The product of record options is ", prod(record_options(times, distances)))
+
+    times, distances = parse_input_kerning(input_lines)
+    print("Part 2: Record options are ", record_options(times, distances)[0])
