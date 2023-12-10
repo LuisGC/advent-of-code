207be002f48e92b651715c7e440326138aa88b4f
Luis GC <luisgc@gmail.com>
2023-12-10 23:15:11 +0100

    2023-10 solved. I don't have energy today to clean the code properly

--- 2023/day-10/main.py
+++ 2023/day-10/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 MAZE_RULES = {
     ("L", 1j): 1, ("L", -1): -1j,
@@ -10,10 +10,9 @@ MAZE_RULES = {
 def starting_position(input_lines: List[str]) -> complex:
     return [complex(j, i) for i, row in enumerate(input_lines) for j, char in enumerate(row) if char == "S"][0]
 
-def distance(maze: List[str], start: complex, initial_direction: int = 1) -> int:
-    dir = initial_direction
-
+def distance_and_track(maze: List[str], start: complex, dir: int = 1) -> Tuple[int, List[complex]]:
     pos = start + dir
+    track = [pos]
     s = 1
 
     while pos != start:
@@ -22,18 +21,91 @@ def distance(maze: List[str], start: complex, initial_direction: int = 1) -> int
             dir = MAZE_RULES[(maze[y][x], dir)]
         
         pos += dir
+        track.append(pos)
         s += 1
 
-    return (s // 2)
+    return (s // 2), track
+
+
+def clean_maze_with_track(maze: List[str], track: List[complex], initial: str) -> List[str]:
+    clean_maze = {}
+    for y, row in enumerate(maze):
+        for x, char in enumerate(row):
+            if char == "." or (complex(x, y) in track):
+                if char == "S":
+                    clean_maze[(x, y)] = initial
+                else:
+                    clean_maze[(x, y)] = char
+            else:
+                # not in track
+                clean_maze[(x, y)] = "."
+
+    return clean_maze
+
+
+def tiles_enclosed(maze: dict, rows: int, cols: int) -> int:
+    enclosed = 0
+    for y in range(rows):
+        inside = False
+        last_corner = None
+        for x in range(cols):
+            pipe = maze.get((x,y))
+            if pipe == ".":
+                if inside:
+                    enclosed += 1
+            elif pipe == "|":
+                assert last_corner is None
+                inside = not inside
+            elif pipe == "-":
+                continue
+            else:
+                if last_corner is None:
+                    assert pipe in ("L", "F")
+                    last_corner = "N" if pipe in ("L", "J") else "S"
+                else:
+                    assert pipe in ("J", "7")
+                    this_corner = "N" if pipe in ("L", "J") else "S"
+                    if last_corner != this_corner:
+                        inside = not inside
+                    last_corner = None
+
+    return enclosed
+
 
 with open("2023/day-10/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-    assert 4 == distance(input_lines, starting_position(input_lines))
+    distance, track = distance_and_track(input_lines, starting_position(input_lines))
+    assert 4 == distance
 
 with open("2023/day-10/example2.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-    assert 8 == distance(input_lines, starting_position(input_lines))
+    distance, track = distance_and_track(input_lines, starting_position(input_lines))
+    assert 8 == distance
     
+with open("2023/day-10/example-enclosed-1.txt", encoding="utf-8") as f:
+    input_lines = [line.strip() for line in f.readlines()]
+    distance, track = distance_and_track(input_lines, starting_position(input_lines))
+    assert 23 == distance
+    clean_maze = clean_maze_with_track(input_lines, track, "F")
+    assert 4 == tiles_enclosed(clean_maze, rows=len(input_lines), cols=len(input_lines[0]))
+
+with open("2023/day-10/example-enclosed-2.txt", encoding="utf-8") as f:
+    input_lines = [line.strip() for line in f.readlines()]
+    distance, track = distance_and_track(input_lines, starting_position(input_lines))
+    assert 70 == distance
+    clean_maze = clean_maze_with_track(input_lines, track, "F")
+    assert 8 == tiles_enclosed(clean_maze, rows=len(input_lines), cols=len(input_lines[0]))
+
+with open("2023/day-10/example-enclosed-3.txt", encoding="utf-8") as f:
+    input_lines = [line.strip() for line in f.readlines()]
+    distance, track = distance_and_track(input_lines, starting_position(input_lines), -1)
+    assert 80 == distance
+    clean_maze = clean_maze_with_track(input_lines, track, "7")
+    assert 10 == tiles_enclosed(clean_maze, rows=len(input_lines), cols=len(input_lines[0]))
+
 with open("2023/day-10/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-    print("Part 1: The farthest distance is ", distance(input_lines, starting_position(input_lines), -1))
+    distance, track = distance_and_track(input_lines, starting_position(input_lines), -1)
+    print("Part 1: The farthest distance is ", distance)
+    clean_maze = clean_maze_with_track(input_lines, track, "J")
+    print("Part 2: Number of tiles enclosed is ", tiles_enclosed(clean_maze, rows=len(input_lines), cols=len(input_lines[0])))
