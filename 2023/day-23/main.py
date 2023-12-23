8c0fe06077d8310d2246e90e846792c346508814
Luis GC <luisgc@gmail.com>
2023-12-23 10:23:12 +0100

    2023-23 part 1 solved

---
+++ 2023/day-23/main.py
@@ -0,0 +1,58 @@
+from typing import List
+from collections import defaultdict
+
+DIRECTIONS = {
+    0: ( 0, -1),  # W
+    1: ( 1,  0),  # S
+    2: ( 0,  1),  # E
+    3: (-1,  0)   # N
+}
+
+def longest_hike(input_lines: List[str], with_slopes: bool = True) -> int:
+
+    memo = defaultdict(lambda: -1)
+
+    queue = [((0, 1), set(), 0)] # position, visited, steps
+
+    while queue:
+        current, queue = queue[0], queue[1:]
+        position, visited, steps = current
+
+        memo[position] = steps
+        row, col = position
+
+        dirs = [dir for dir in DIRECTIONS.values()]
+
+        if with_slopes and input_lines[row][col] == ">":
+            dirs = [DIRECTIONS[2]]
+        elif with_slopes and input_lines[row][col] == "<":
+            dirs = [DIRECTIONS[0]]
+        elif with_slopes and input_lines[row][col] == "^":
+            dirs = [DIRECTIONS[3]]
+        elif with_slopes and input_lines[row][col] == "v":
+            dirs = [DIRECTIONS[1]]
+        
+        for new_dir in dirs:
+            new_row = position[0] + new_dir[0]
+            new_col = position[1] + new_dir[1]
+            new_pos = (new_row, new_col)
+            if (new_pos not in visited and
+                0 <= new_row < len(input_lines) and 0 <= new_col < len(input_lines[0]) and
+                input_lines[new_row][new_col] != "#" and
+                memo[position] < steps + 1):
+                queue.append((new_pos, visited | {position}, steps + 1))
+
+    return memo[((len(input_lines)-1), len(input_lines[-1])-2)]
+
+with open("2023/day-23/example.txt", encoding="utf-8") as f:
+    input_lines = [line.strip() for line in f.readlines()]
+    
+    assert 94 == longest_hike(input_lines)
+    assert 154 == longest_hike(input_lines, with_slopes=False)
+
+with open("2023/day-23/input.txt", encoding="utf-8") as f:
+    input_lines = [line.strip() for line in f.readlines()]
+    
+    print("Part 1: The longest hike is ", longest_hike(input_lines))
+    print("Part 2: The longest hike without slopes is ", longest_hike(input_lines, with_slopes=False))
+    
