4f0fa290dcd8bcb49607c9f99a29ebf5fb7c7e32
Luis GC <luisgc@gmail.com>
2023-12-25 09:48:20 +0100

    2023-25 cleaning code and adding perf metrics

--- 2023/day-25/main.py
+++ 2023/day-25/main.py
@@ -1,6 +1,17 @@
 from typing import List
 from math import prod
 from networkx import Graph, connected_components, minimum_edge_cut
+from time import perf_counter
+
+def profiler(method):
+    def wrapper_method(*arg, **kw):
+        t = perf_counter()
+        ret = method(*arg, **kw)
+        print("Method " + method.__name__ + " took: " + "{:2.5f}".format(perf_counter() - t) + " sec") 
+        return ret
+    
+    return wrapper_method
+
 
 def parse_input(lines: List[str]) -> Graph:
     g = Graph()
@@ -11,14 +22,17 @@ def parse_input(lines: List[str]) -> Graph:
 
     return g
 
-with open("2023/day-25/example.txt", encoding="utf-8") as f:
-    input_lines = [line.strip() for line in f.readlines()]
+@profiler
+def disconnect_half_equipment(lines: List[str]) -> int:
     graph = parse_input(input_lines)
     graph.remove_edges_from(minimum_edge_cut(graph))
-    assert 54 == prod(len(comp) for comp in connected_components(graph))
+
+    return prod(len(comp) for comp in connected_components(graph))
+
+with open("2023/day-25/example.txt", encoding="utf-8") as f:
+    input_lines = [line.strip() for line in f.readlines()]
+    assert 54 == disconnect_half_equipment(input_lines)
 
 with open("2023/day-25/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-    graph = parse_input(input_lines)
-    graph.remove_edges_from(minimum_edge_cut(graph))
-    print("Part 1: The product of the sizes is ", prod(len(comp) for comp in connected_components(graph)))
+    print("Part 1: The product of the sizes is ", disconnect_half_equipment(input_lines))
