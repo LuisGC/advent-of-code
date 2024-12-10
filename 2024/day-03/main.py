35a00d4a7c58293891199052dfd96f223369a8a9
Luis GC <luisgc@gmail.com>
2024-12-10 19:42:27 +0100

    2024-03 part 2

--- 2024/day-03/main.py
+++ 2024/day-03/main.py
@@ -1,21 +1,32 @@
 import re
 
-mul_pattern = r"mul\((\d+),(\d+)\)"
+def adding_muls(lines: str, with_flags: bool= False) -> int:
 
-def adding_muls(lines: list) -> int:
+    total = 0
+    if with_flags:
+        enabled = True
+        matches = re.findall(r"(mul\((\d+),(\d+)\)|do\(\)|don\'t(\))", lines)
+        for m in matches:
+            if m[0] == 'don\'t()':
+                enabled = False
+            elif m[0] == 'do()':
+                enabled = True
+            if enabled and 'mul' in m[0]:
+                total += int(m[1]) * int(m[2])
+    else:
+        matches = re.findall(r"mul\((\d+),(\d+)\)", lines)
 
-    matches = re.findall(mul_pattern, lines)
-
-    total = sum(int(x) * int (y) for x, y in matches)
+        total = sum(int(x) * int (y) for x, y in matches)
 
     return total
 
 with open("2024/day-03/example.txt", encoding="utf-8") as f:
     input_lines = f.read()
     assert 161 == adding_muls(input_lines)
+    assert 48 == adding_muls(input_lines, with_flags=True)
 
 with open("2024/day-03/input.txt", encoding="utf-8") as f:
     input_lines = f.read()
 
     print(f"Part 1: Adding the muls is {adding_muls(input_lines)}")
-#    print(f"Part 2: Amount of safe lines with tolerance is {safe_lines(input_lines, tolerate_bad = True)}")
+    print(f"Part 2: Adding the muls with dos and donts is {adding_muls(input_lines, with_flags = True)}")
