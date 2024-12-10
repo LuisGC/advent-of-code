6bae57c152e4501e2ac65ac953bc0b85d592e7b4
Luis GC <luisgc@gmail.com>
2024-12-10 20:13:27 +0100

    2024-04 solved

--- 2024/day-04/main.py
+++ 2024/day-04/main.py
@@ -25,12 +25,33 @@ def count_XMAS(lines: list) -> int:
 
     return xmas_count
 
+def count_X_MAS(lines: list) -> int:
+
+    rows = len(lines)
+    cols = len(lines[0])
+
+    xmas_count = 0
+    for r in range(rows):
+        for c in range(cols):
+            if r+2<rows and c+2<cols:
+                if lines[r][c]=='M' and lines[r+1][c+1]=='A' and lines[r+2][c+2]=='S' and lines[r+2][c]=='M' and lines[r][c+2]=='S':
+                    xmas_count += 1
+                if lines[r][c]=='M' and lines[r+1][c+1]=='A' and lines[r+2][c+2]=='S' and lines[r+2][c]=='S' and lines[r][c+2]=='M':
+                    xmas_count += 1
+                if lines[r][c]=='S' and lines[r+1][c+1]=='A' and lines[r+2][c+2]=='M' and lines[r+2][c]=='M' and lines[r][c+2]=='S':
+                    xmas_count += 1
+                if lines[r][c]=='S' and lines[r+1][c+1]=='A' and lines[r+2][c+2]=='M' and lines[r+2][c]=='S' and lines[r][c+2]=='M':
+                    xmas_count += 1
+
+    return xmas_count
+
 with open("2024/day-04/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     assert 18 == count_XMAS(input_lines)
+    assert 9 == count_X_MAS(input_lines)
 
 with open("2024/day-04/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
 
     print(f"Part 1: XMAS found times is {count_XMAS(input_lines)}")
-#    print(f"Part 2: Amount of safe lines with tolerance is {safe_lines(input_lines, tolerate_bad = True)}")
+    print(f"Part 2: X-MAS found times is {count_X_MAS(input_lines)}")
