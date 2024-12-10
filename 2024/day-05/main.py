5300cd3486ad5ba5b6fa4975c5ead2b517833361
Luis GC <luisgc@gmail.com>
2024-12-10 21:13:52 +0100

    2024-05 solved

--- 2024/day-05/main.py
+++ 2024/day-05/main.py
@@ -1,6 +1,15 @@
-def sum_middle_pages(lines: list) -> int:
+from functools import cmp_to_key
+
+def sum_middle_pages(lines: list, with_fix: bool = False) -> int:
+
+    def compare(x, y):
+        if (x,y) in dd: return -1
+        if (y, x) in dd: return 1
+        return 0
+    
     result = 0
     dict = {}
+    dd = set()
 
     for line in lines:
         if '|' in line:
@@ -9,6 +18,7 @@ def sum_middle_pages(lines: list) -> int:
                 dict[left].append(right)
             else:
                 dict[left] = [right]
+            dd.add((left, right))
         if ',' in line:
             pages = line.replace("\n", "").split(",")
             done = set()
@@ -19,7 +29,10 @@ def sum_middle_pages(lines: list) -> int:
                         if after in done:
                             good = False
                 done.add(page)
-            if good:
+            if not with_fix and good:
+                result += int(pages[len(pages)//2])
+            elif not good and with_fix:
+                pages.sort(key=cmp_to_key(compare))
                 result += int(pages[len(pages)//2])
 
     return result
@@ -27,9 +40,10 @@ def sum_middle_pages(lines: list) -> int:
 with open("2024/day-05/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     assert 143 == sum_middle_pages(input_lines)
+    assert 123 == sum_middle_pages(input_lines, with_fix=True)
 
 with open("2024/day-05/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
 
     print(f"Part 1: Sum of middle page numbers is {sum_middle_pages(input_lines)}")
-#    print(f"Part 2: X-MAS found times is {count_X_MAS(input_lines)}")
+    print(f"Part 2: Sum of middle page numbers with fix is {sum_middle_pages(input_lines, with_fix=True)}")
