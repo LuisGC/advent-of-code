f2ceca3e1344f53f323a28b8fa6cc7a252bc9fb1
Luis GC <luisgc@gmail.com>
2024-12-11 21:16:20 +0100

    2024-09 solved

--- 2024/day-09/main.py
+++ 2024/day-09/main.py
@@ -22,35 +22,45 @@ class Block:
 class Disk:
     blocks: Sequence[Block]
 
-    def compact(self) -> None:
-        i = 0
-        j = len(self.blocks) - 1
+    def compact(self, frag: bool = True) -> None:
+        if frag:
+            i = 0
+            j = len(self.blocks) - 1
 
-        while i<j:
-            first_block = self.blocks[i]
-            last_block = self.blocks[j]
+            while i<j:
+                first_block = self.blocks[i]
+                last_block = self.blocks[j]
 
-            if first_block.remaining == 0:
-                i += 1
-                continue
+                if first_block.remaining == 0:
+                    i += 1
+                    continue
 
-            if not last_block.files:
-                j -= 1
-                continue
+                if not last_block.files:
+                    j -= 1
+                    continue
 
-            last_file_size = last_block.files[-1].size
+                last_file_size = last_block.files[-1].size
 
-            if last_file_size <= first_block.remaining:
-                first_block.files.append(last_block.files.pop())
-                first_block.remaining -= last_file_size
-                last_block.remaining += last_file_size
-            else:
-                first_block.files.append(
-                    File(id=last_block.files[-1].id, size=first_block.remaining)
-                )
-                last_block.files[-1].size -= first_block.remaining
-                last_block.remaining += first_block.remaining
-                first_block.remaining = 0
+                if last_file_size <= first_block.remaining:
+                    first_block.files.append(last_block.files.pop())
+                    first_block.remaining -= last_file_size
+                    last_block.remaining += last_file_size
+                else:
+                    first_block.files.append(
+                        File(id=last_block.files[-1].id, size=first_block.remaining)
+                    )
+                    last_block.files[-1].size -= first_block.remaining
+                    last_block.remaining += first_block.remaining
+                    first_block.remaining = 0
+        else:
+            for j in reversed(range(1, len(self.blocks))):
+                for i in range(j):
+                    last_file_size = self.blocks[j].files[0].size
+                    if last_file_size <= self.blocks[i].remaining:
+                        self.blocks[i].files.append(self.blocks[j].files.popleft())
+                        self.blocks[i].remaining -= last_file_size
+                        self.blocks[j - 1].remaining += last_file_size
+                        break
 
     def checksum(self) -> int:
         res = 0
@@ -85,9 +95,16 @@ with open("2024/day-09/example.txt", encoding="utf-8") as f:
     disk.compact()
     assert 1928 == disk.checksum()
 
+    disk = Disk.from_str(list(map(int, input)))
+    disk.compact(frag=False)
+    assert 2858 == disk.checksum()
+
 with open("2024/day-09/input.txt", encoding="utf-8") as f:
     input = f.read().rstrip()
     disk = Disk.from_str(list(map(int, input)))
     disk.compact()
     print(f"Part 1: Disk checksum is {disk.checksum()}")
-#    print(f"Part 2: Amount of rocks after 75 blinks is {stones_produced(rocks, 75)}")
+    
+    disk = Disk.from_str(list(map(int, input)))
+    disk.compact(frag=False)
+    print(f"Part 2: Disk checksum without fragmentation is {disk.checksum()}")
