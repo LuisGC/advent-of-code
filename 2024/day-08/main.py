1b68baa92fb8211e76f33059588628974b8019e8
Luis GC <luisgc@gmail.com>
2024-12-13 16:10:29 +0100

    2024-08 solved

--- 2024/day-08/main.py
+++ 2024/day-08/main.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from typing import Tuple
-from itertools import permutations
+from itertools import permutations, count
 
 def parse_input(lines: str) -> Tuple[set, defaultdict]:
     grid = {
@@ -15,20 +15,35 @@ def parse_input(lines: str) -> Tuple[set, defaultdict]:
 
     return set(grid), antennas
 
-def find_antinodes(grid: set, antennas: defaultdict) -> int:
+def find_antinodes(grid: set, antennas: defaultdict) -> set:
     return grid &  {
         (2 * row - row_other, 2 * col - col_other)
         for (row, col), (row_other, col_other) in permutations(antennas, r=2)
     }
 
-def unique_locations(grid: set, antennas: defaultdict) -> int:
-    return len(set.union(*[find_antinodes(grid, pos) for pos in antennas.values()]))
+def find_all_antinodes(grid: set, antennas: defaultdict) -> set:
+    antinodes = set()
+    for (row, col), (row_other, col_other) in permutations(antennas, r=2):
+        dr, dc = row_other - row, col_other - col
+        for n in count(1):
+            if (antinode := (row + n * dr, col + n * dc)) not in grid:
+                break
+            antinodes.add(antinode)
+    return antinodes
+
+def unique_locations(grid: set, antennas: defaultdict, with_harmonics: bool = False) -> int:
+    if with_harmonics:
+        return len(set.union(*[find_all_antinodes(grid, pos) for pos in antennas.values()]))    
+    else:
+        return len(set.union(*[find_antinodes(grid, pos) for pos in antennas.values()]))
 
 
 with open("2024/day-08/example.txt", encoding="utf-8") as f:
     grid, antennas = parse_input(f.read().rstrip())
     assert 14 == unique_locations(grid, antennas)
+    assert 34 == unique_locations(grid, antennas, with_harmonics=True)
 
 with open("2024/day-08/input.txt", encoding="utf-8") as f:
     grid, antennas = parse_input(f.read().rstrip())
     print(f"Part 1: Amount of unique locations is {unique_locations(grid, antennas)}")
+    print(f"Part 2: Amount of unique locations with harmonics is {unique_locations(grid, antennas, with_harmonics=True)}")
