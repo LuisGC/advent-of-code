ddaf5f45f0711c87b8392a4bc28864a4fe304967
Luis GC <luisgc@gmail.com>
2024-12-16 23:58:09 +0100

    2024-06 solved

--- 2024/day-06/main.py
+++ 2024/day-06/main.py
@@ -33,16 +33,36 @@ def simulate_guard(map_data: List[str], start_pos: Tuple[int, int], start_dir: T
     
     return visited, True
 
+def simulate_guard_with_obstruction(map_data: List[str], start_pos: Tuple[int, int], start_dir: Tuple[int, int], obstruction_pos: Tuple[int, int]) -> bool:
+    map_data[obstruction_pos[0]][obstruction_pos[1]] = '#'
+    _, loop_detected = simulate_guard(map_data, start_pos, start_dir)
+    map_data[obstruction_pos[0]][obstruction_pos[1]] = '.' # Reset the position
+    return loop_detected
+
+@profiler
+def valid_obstruction_positions(map_data: List[str], visited_positions: set, start_pos: Tuple[int, int], start_dir: Tuple[int, int]) -> int:
+    valid_obstruction_positions = 0
+    for pos in visited_positions:
+        if pos == start_pos:
+            continue
+        if simulate_guard_with_obstruction(map_data, start_pos, start_dir, pos):
+            valid_obstruction_positions += 1
+
+    return valid_obstruction_positions
+
+
 with open("2024/day-06/example.txt", encoding="utf-8") as f:
     map_data = [list(line.strip()) for line in f]
     start_pos, start_dir = find_guard(map_data)
-    visited_positions, loop_detected = simulate_guard(map_data, start_pos, start_dir)
+    visited_positions, _ = simulate_guard(map_data, start_pos, start_dir)
 
     assert 41 == len(visited_positions)
+    assert 6 == valid_obstruction_positions(map_data, visited_positions, start_pos, start_dir)
 
 with open("2024/day-06/input.txt", encoding="utf-8") as f:
     map_data = [list(line.strip()) for line in f]
     start_pos, start_dir = find_guard(map_data)
-    visited_positions, loop_detected = simulate_guard(map_data, start_pos, start_dir)
+    visited_positions, _ = simulate_guard(map_data, start_pos, start_dir)
 
     print(f"Part 1: Number of distinct positions visited is {len(visited_positions)}")
+    print(f"Part 2: Number of valid obstruction positions is {valid_obstruction_positions(map_data, visited_positions, start_pos, start_dir)}")
