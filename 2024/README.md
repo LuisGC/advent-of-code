5717f711e0cc87b5d68da5e6f8eb92af4972b443
Luis GC <luisgc@gmail.com>
2024-12-18 23:00:27 +0100

    2024-18 solved

--- 2024/README.md
+++ 2024/README.md
@@ -8,5 +8,6 @@
 | 12 | 232.74838 sec | N/A |
 | 14 | 0.00054 sec | 0.05635 sec |
 | 16 | 0.04633 sec | 0.72491 sec |
+| 18 | 0.00015 sec | 18.74259 sec |
 
 More stats in [Advent of Code Personal Times](https://adventofcode.com/2024/leaderboard/self)
--- 2024/day-18/main.py
+++ 2024/day-18/main.py
@@ -1,12 +1,12 @@
 import sys
 import heapq
-from typing import List
+from typing import List, Tuple
 sys.path.insert(0, './')
 from utils import profiler, DIRECTIONS
 
 EMPTY, CORRUPTED = ".#"
 
-def parse_input(lines: List[str], memory_size: int, num_bytes: str) -> List[List[str]]:
+def parse_input(lines: List[str], memory_size: int, num_bytes: str) -> Tuple[List[List[str]], List[Tuple[int, int]]]:
     falling_bytes = []
     for line in lines:
         x, y = line.split(",")
@@ -16,9 +16,8 @@ def parse_input(lines: List[str], memory_size: int, num_bytes: str) -> List[List
         x, y = falling_bytes[i]
         grid[y][x] = CORRUPTED
 
-    return grid
+    return grid, falling_bytes
 
-@profiler
 def shortest_path(grid: List[List[str]], memory_size: int) -> int:
     start = (0, 0)
     end = (memory_size, memory_size)
@@ -36,19 +35,31 @@ def shortest_path(grid: List[List[str]], memory_size: int) -> int:
         
         for direction in DIRECTIONS:
             next_x, next_y = (location[0] + direction[0], location[1] + direction[1])
-            if (0 <= next_x < memory_size + 1) and (0 <= next_y < memory_size + 1) and grid[next_x][next_y] != CORRUPTED:
+            if (0 <= next_x < memory_size + 1) and (0 <= next_y < memory_size + 1) and grid[next_y][next_x] != CORRUPTED:
                 if (next_x, next_y) not in visited:
                     heapq.heappush(queue, (distance + 1, (next_x, next_y)))
+
+@profiler
+def first_blocking_byte(grid: List[List[str]], falling_bytes: List[Tuple[int, int]], memory_size: int, num_bytes: str) -> int:
+
+    for i in range(num_bytes + 1, len(falling_bytes)):
+        x, y = falling_bytes[i]
+        grid[y][x] = CORRUPTED
+        if shortest_path(grid, memory_size) is None:
+            break
+    return (x, y)
 
 
 with open("2024/day-18/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-    grid = parse_input(input_lines, 6, 12)
+    grid, falling_bytes = parse_input(input_lines, 6, 12)
 
     assert 22 == shortest_path(grid, 6)
+    assert (6,1) == first_blocking_byte(grid, falling_bytes, 6, 12)
 
 with open("2024/day-18/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
-    grid = parse_input(input_lines, 70, 1024)
+    grid, falling_bytes = parse_input(input_lines, 70, 1024)
 
-    print(f"Part 1: Minimum number of steps is {shortest_path(grid, 70)}")
+    first = first_blocking_byte(grid, falling_bytes, 70, 1024)
+    print(f"Part 1: Minimum number of steps is {",".join(map(str, first))}")
