054962deb884e5d1170b7d20d7137ae6842b7482
Luis GC <luisgc@gmail.com>
2024-12-23 11:05:41 +0100

    2024-23 solved

--- 2024/day-23/main.py
+++ 2024/day-23/main.py
@@ -35,6 +35,26 @@ def lan_parties_filter(lan_dict: defaultdict, code: str = "t") -> int:
 
     return len([party for party in lan_parties if any(member.startswith(code) for member in party.members)])
 
+def membersInterconnected(lan_party: LanGroup) -> bool:
+    members = lan_party.members
+    for member in members:
+        my_connections = lan_dict[member]
+        for other in members:
+            if other not in my_connections and other != member:
+                return False
+    return True
+
+def largest_party(lan_dict: defaultdict) -> str:
+    lan_parties = set()
+    for me, mine in lan_dict.items():
+        for other in mine:
+            lan_parties.add(LanGroup([me, other] + [x for x in lan_dict[other] if x in mine]))
+
+    interconnected_parties = [it.members for it in (filter(membersInterconnected, list(lan_parties)))]
+    largest_party = sorted(interconnected_parties, key=lambda party: len(party)).pop()
+
+    return ",".join(largest_party)
+
 
 with open("2024/day-23/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
@@ -42,8 +62,11 @@ with open("2024/day-23/example.txt", encoding="utf-8") as f:
 
     assert 7 == lan_parties_filter(lan_dict)
 
+    assert "co,de,ka,ta" == largest_party(lan_dict)
+
 with open("2024/day-23/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     lan_dict = parse_input(input_lines)
 
-    print(f"Part 1: Minimum number of steps is {lan_parties_filter(lan_dict)}")
+    print(f"Part 1: Lan parties with filter is {lan_parties_filter(lan_dict)}")
+    print(f"Part 2: Largest interconnected LAN party is {largest_party(lan_dict)}")
