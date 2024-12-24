596a6e833a9a7ebc7fb6a96f6b9762603d1039be
Luis GC <luisgc@gmail.com>
2024-12-24 13:08:59 +0100

    2024-24 solved!

--- 2024/README.md
+++ 2024/README.md
@@ -12,5 +12,6 @@
 | 20 | 0.16352 sec | 9.02489 sec |
 | 21 | 0.00947 sec | 0.03461 sec |
 | 22 | 4.46105 sec | 8.02275 sec |
+| 24 | 0.00038 sec | 6.38525 sec |
 
 More stats in [Advent of Code Personal Times](https://adventofcode.com/2024/leaderboard/self)
--- 2024/day-24/main.py
+++ 2024/day-24/main.py
@@ -1,4 +1,8 @@
-from typing import Tuple, List
+from itertools import combinations
+from typing import Tuple, Optional
+import sys
+sys.path.insert(0, './')
+from utils import profiler
 
 def parse_input(input_file: str) -> Tuple[dict, dict]:
     content = [s.strip().split("\n") for s in input_file.rstrip().split("\n\n")]
@@ -22,7 +26,7 @@ def get_state(key: str, states: dict, gates: dict) -> bool:
             raise ValueError(f"Unknown operator {operator}")
     return states[key]
 
-
+@profiler
 def decimal_output(states: dict, gates: dict) -> int:
     num = ''
     key = 0
@@ -38,6 +42,58 @@ def decimal_output(states: dict, gates: dict) -> int:
             break
     return int(num, 2)
 
+def furthest_made(gates: dict, max_z: int) -> Tuple[int, set]:
+    ops = {}
+    for res, (x1, op, x2) in gates.items():
+        ops[(frozenset([x1, x2]), op)] = res
+
+    def get_res(x1_, x2_, op_) -> Optional[int]:
+        return ops.get((frozenset([x1_, x2_]), op_), None)
+
+    carries = {}
+    correct = set()
+    prev_intermediaries = set()
+    for i in range(max_z):
+        prev_digit = get_res(f"x{i:02d}", f"y{i:02d}", "XOR")
+        prev_carry1 = get_res(f"x{i:02d}", f"y{i:02d}", "AND")
+        if i == 0:
+            carries[i] = prev_carry1
+            continue
+        digit = get_res(carries[i - 1], prev_digit, "XOR")
+        if digit != f"z{i:02d}":
+            return i - 1, correct
+        correct.add(carries[i - 1])
+        correct.add(prev_digit)
+        for wire in prev_intermediaries:
+            correct.add(wire)
+        
+        prev_carry2 = get_res(carries[i - 1], prev_digit, "AND")
+        carry_out = get_res(prev_carry1, prev_carry2, "OR")
+        carries[i] = carry_out
+
+        prev_intermediaries = {prev_carry1, prev_carry2}
+
+    return max_z, correct
+    
+@profiler
+def swapping_gates(gates: dict, max_z: int = 45) -> int:
+    swaps = set()
+    base, used = furthest_made(gates, max_z)
+
+    for _ in range(4):
+        for res_i, res_j in combinations(gates.keys(), 2):
+            if "z00" in (res_i, res_j) or res_i in used or res_j in used:
+                continue
+            gates[res_i], gates[res_j] = gates[res_j], gates[res_i]
+            attempt, attempt_used = furthest_made(gates, max_z)
+            if attempt > base:
+                swaps.add((res_i, res_j))
+                base, used = attempt, attempt_used
+                break
+            gates[res_i], gates[res_j] = gates[res_j], gates[res_i]
+
+    return ",".join(sorted(sum(swaps, start=tuple())))
+
 
 with open("2024/day-24/example.txt", encoding="utf-8") as f:
     states, gates = parse_input(f.read())
@@ -49,7 +105,13 @@ with open("2024/day-24/larger-example.txt", encoding="utf-8") as f:
     
     assert 2024 == decimal_output(states, gates)
 
+with open("2024/day-24/example-2.txt", encoding="utf-8") as f:
+    states, gates = parse_input(f.read())
+    
+    assert 9 == decimal_output(states, gates)
+
 with open("2024/day-24/input.txt", encoding="utf-8") as f:
     states, gates = parse_input(f.read())
 
     print(f"Part 1: Sum of possible designs is {decimal_output(states, gates)}")
+    print(f"Part 2: Swapping gates and sorting result: {swapping_gates(gates, max_z=45)}")
