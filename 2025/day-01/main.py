f7608b3e5b9da93e66824ad99cbbbfac97f4d3c5
Luis GC <luisgc@gmail.com>
2025-12-01 23:35:32 +0100

    2025.01 solved

--- 2025/day-01/main.py
+++ 2025/day-01/main.py
@@ -1,14 +1,18 @@
-def extract_password(input_lines: list[str]) -> int:
-    password = 0
+def extract_password(input_lines: list[str], new_method: bool = False) -> int:
+    password, new_password = 0, 0
     position = 50
     
     for line in input_lines:
         if line:
             for i in range(int(line[1:])):
                 position = (position + [-1, 1][line[0] == 'R']) % 100
-
+                new_password += position == 0
             password += position == 0
-    return password
+    
+    if new_method:
+        return new_password
+    else:
+        return password
 
 with open("2025/day-01/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
@@ -18,4 +22,4 @@ with open("2025/day-01/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
 
     print(f"Part 1: The password is {extract_password(input_lines)}")
-#    print(f"Part 2: Sum of all sim scores is {sim_score(input_lines)}")
+    print(f"Part 2: The new password is {extract_password(input_lines, new_method=True)}")
