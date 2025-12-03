deb0d0181da3b67d9018a28f7618f24466dac450
Luis GC <luisgc@gmail.com>
2025-12-03 08:15:33 +0100

    2025-03 solved

--- 2025/day-03/main.py
+++ 2025/day-03/main.py
@@ -1,10 +1,10 @@
-def largest_joltage(input: list[list[int]]) -> int:
+def largest_joltage(input: list[list[int]], length: int) -> int:
     largest = 0
     for line in input:
         best: list[int] = []
         remaining = line
-        for i in range(2):
-            best_digit = max(remaining[:len(remaining) + i + 1 - 2])
+        for i in range(length):
+            best_digit = max(remaining[:len(remaining) + i + 1 - length])
             best_index = remaining.index(best_digit)
             best.append(best_digit)
             remaining = remaining[best_index + 1 :]
@@ -15,10 +15,11 @@ def largest_joltage(input: list[list[int]]) -> int:
 
 with open("2025/day-03/example.txt", encoding="utf-8") as f:
     input_lines = [[int(char) for char in line.strip()] for line in f.readlines()]
-    assert 357 == largest_joltage(input_lines)
+    assert 357 == largest_joltage(input_lines, length=2)
+    assert 3121910778619 == largest_joltage(input_lines, length=12)
 
 with open("2025/day-03/input.txt", encoding="utf-8") as f:
     input_lines = [[int(char) for char in line.strip()] for line in f.readlines()]
 
-    print(f"Part 1: The largest joltage is {largest_joltage(input_lines)}")
-#    print(f"Part 2: The largest joltage is {largest_joltage(input_lines)}")
+    print(f"Part 1: The largest joltage is {largest_joltage(input_lines, length=2)}")
+    print(f"Part 2: The largest joltage is {largest_joltage(input_lines, length=12)}")
