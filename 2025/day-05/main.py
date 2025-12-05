df0f07b601b571f1d3e8dfd3d1754934f5262a0d
Luis GC <luisgc@gmail.com>
2025-12-05 08:40:10 +0100

    2025-05 solved

--- 2025/day-05/main.py
+++ 2025/day-05/main.py
@@ -1,6 +1,7 @@
-def parse_input(input_lines):
+def parse_input(input_lines) -> tuple[list[tuple[int, int]], list[int]]:
     ranges = []
     ingredients = []
+
     parsing_ranges = True
 
     for line in input_lines:
@@ -30,12 +31,32 @@ def count_fresh(ranges, ingredients) -> int:
 
     return fresh_count
 
+def count_all_fresh(ranges) -> int:
+
+    #removing overlapping ranges
+    ranges.sort()
+
+    i = 0
+    while i < len(ranges) - 1:
+        current_start, current_end = ranges[i]
+        next_start, next_end = ranges[i + 1]
+
+        if next_start <= current_end:
+            ranges[i] = (current_start, max(current_end, next_end))
+            ranges.pop(i + 1)
+        else:
+            i += 1
+    
+    return sum([end - start + 1 for start, end in ranges])
+
+
 with open("2025/day-05/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
 
     ranges, ingredients = parse_input(input_lines)
-    
+
     assert 3 == count_fresh(ranges, ingredients)
+    assert 14 == count_all_fresh(ranges)
 
 with open("2025/day-05/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
@@ -43,4 +64,4 @@ with open("2025/day-05/input.txt", encoding="utf-8") as f:
     ranges, ingredients = parse_input(input_lines)
 
     print(f"Part 1: The amount of fresh ingredients is {count_fresh(ranges, ingredients)}")
-#    print(f"Part 2: The amount of accessible rolls removing is {accessible_rolls(input_lines, removing=True)}")
+    print(f"Part 2: The total amount of fresh ingredients is {count_all_fresh(ranges)}")
