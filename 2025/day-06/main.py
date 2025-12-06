62041857a8d1ff88a5cca428c5d13e9ce68d40f5
Luis GC <luisgc@gmail.com>
2025-12-06 07:33:36 +0100

    2025-06 solved

--- 2025/day-06/main.py
+++ 2025/day-06/main.py
@@ -20,13 +20,48 @@ def calculate_all(lines: list[str]) -> int:
 
     return total
 
+def calculate_r2l(lines: list[str]) -> int:
+    rows, cols = len(lines), len(lines[0]) - 1
+
+    total = 0
+    numbers = []
+
+    for c in range(cols - 1, -1, -1):
+        current = 0
+
+        for r in range(rows):
+            x = lines[r][c]
+
+            if x.isnumeric():
+                current *= 10
+                current += int(x)
+
+            if r == rows - 1 and current != 0:
+                numbers.append(current)
+                current = 0
+
+            if x in "+*":
+                acc = 1 if x == "*" else 0
+
+                for n in numbers:
+                    if x == "*":
+                        acc *= n
+                    else:
+                        acc += n
+                
+                total += acc
+                numbers = []
+
+    return total
+
 with open("2025/day-06/example.txt", encoding="utf-8") as f:
-    input_lines = [line.strip() for line in f.readlines()]
+    input_lines = [line for line in f.readlines()]
 
     assert 4277556 == calculate_all(input_lines)
+    assert 3263827 == calculate_r2l(input_lines)
 
 with open("2025/day-06/input.txt", encoding="utf-8") as f:
-    input_lines = [line.strip() for line in f.readlines()]
+    input_lines = [line for line in f.readlines()]
 
     print(f"Part 1: The grand total is {calculate_all(input_lines)}")
-#    print(f"Part 2: The grand total is {count_all_fresh(ranges)}")
+    print(f"Part 2: The grand total in Cephalopod Math is {calculate_r2l(input_lines)}")
