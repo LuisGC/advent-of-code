ad7cc0fb9eec25133181d8b2322983b5dae315ea
Luis GC <luisgc@gmail.com>
2025-12-08 09:25:55 +0100

    2025-08 solved

--- 2025/day-08/main.py
+++ 2025/day-08/main.py
@@ -3,7 +3,7 @@ from math import dist
 
 def connect_circuits(boxes: list[tuple[int]], iterations: int) -> tuple[int, int]:
     circuits = []
-    prod = last_cable = 1
+    prod_three_largest = prod_last_cable = 1
 
     distances = [(*pair, dist(*pair)) for pair in combinations(boxes, 2)]
 
@@ -25,28 +25,28 @@ def connect_circuits(boxes: list[tuple[int]], iterations: int) -> tuple[int, int
 
         if i == iterations -1:
             lengths = sorted([len(circuit) for circuit in circuits], reverse=True)[:3]
-            prod = lengths[0] * lengths[1] * lengths[2]
+            prod_three_largest = lengths[0] * lengths[1] * lengths[2]
 
         if len(circuits) == 1 and len(circuits[0]) == len(boxes):
-            last_cable = p[0] * q[0]
+            prod_last_cable = p[0] * q[0]
             break
 
-    return prod, last_cable
+    return prod_three_largest, prod_last_cable
 
 with open("2025/day-08/example.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     boxes = [tuple(int(pos) for pos in line.split(",")) for line in input_lines]
 
-    prod, last_cable = connect_circuits(boxes, 10)
+    prod_three_largest, prod_last_cable = connect_circuits(boxes, 10)
 
-    assert 40 == prod
-    assert 25272 == last_cable
+    assert 40 == prod_three_largest
+    assert 25272 == prod_last_cable
 
 with open("2025/day-08/input.txt", encoding="utf-8") as f:
     input_lines = [line.strip() for line in f.readlines()]
     boxes = [tuple(int(pos) for pos in line.split(",")) for line in input_lines]
 
-    prod, last_cable = connect_circuits(boxes, 1000)
+    prod_three_largest, prod_last_cable = connect_circuits(boxes, 1000)
 
-    print(f"Part 1: The prod of the sizes of the three largest circuits is {prod}")
-    print(f"Part 2: The prod of x coordinates of the last cable is {last_cable}")
+    print(f"Part 1: The prod of the sizes of the three largest circuits is {prod_three_largest}")
+    print(f"Part 2: The prod of x coordinates of the last cable is {prod_last_cable}")
