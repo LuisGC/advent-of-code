016dd60f511afa6bf2764c3c97ae92dc588ebc35
Luis GC <luisgc@gmail.com>
2025-12-08 09:24:28 +0100

    2025-08 solved

---
+++ 2025/day-08/main.py
@@ -0,0 +1,52 @@
+from itertools import combinations
+from math import dist
+
+def connect_circuits(boxes: list[tuple[int]], iterations: int) -> tuple[int, int]:
+    circuits = []
+    prod = last_cable = 1
+
+    distances = [(*pair, dist(*pair)) for pair in combinations(boxes, 2)]
+
+    for i, (p, q, _) in enumerate(sorted(distances, key=lambda x: x[2])):
+        s = {p, q}
+
+        for circuit in circuits:
+            if p not in circuit and q not in circuit:
+                continue
+
+            temp = [c for c in circuits if p not in c and q not in c]
+            for box in filter(lambda c: p in c or q in c, circuits):
+                s |= box
+            temp.append(s)
+            circuits = temp
+            break
+        else:
+            circuits.append(set([p, q]))
+
+        if i == iterations -1:
+            lengths = sorted([len(circuit) for circuit in circuits], reverse=True)[:3]
+            prod = lengths[0] * lengths[1] * lengths[2]
+
+        if len(circuits) == 1 and len(circuits[0]) == len(boxes):
+            last_cable = p[0] * q[0]
+            break
+
+    return prod, last_cable
+
+with open("2025/day-08/example.txt", encoding="utf-8") as f:
+    input_lines = [line.strip() for line in f.readlines()]
+    boxes = [tuple(int(pos) for pos in line.split(",")) for line in input_lines]
+
+    prod, last_cable = connect_circuits(boxes, 10)
+
+    assert 40 == prod
+    assert 25272 == last_cable
+
+with open("2025/day-08/input.txt", encoding="utf-8") as f:
+    input_lines = [line.strip() for line in f.readlines()]
+    boxes = [tuple(int(pos) for pos in line.split(",")) for line in input_lines]
+
+    prod, last_cable = connect_circuits(boxes, 1000)
+
+    print(f"Part 1: The prod of the sizes of the three largest circuits is {prod}")
+    print(f"Part 2: The prod of x coordinates of the last cable is {last_cable}")
